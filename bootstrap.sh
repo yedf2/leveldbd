95b024a106076f6ba53544a3d1fd9c9436355be2
yedf <dongfuye@163.com>
2015-05-31 11:59:40 +0800

    change leveldb repo to google/leveldb

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ git clone https://github.com/google/snappy &&
 cd snappy && ./autogen.sh && ./configure && make clean && make $1 || exit 1
 cd ..
 
-git clone https://github.com/yedf/leveldb &&
+git clone https://github.com/google/leveldb &&
 cd leveldb && make libleveldb.a $1 || exit 1
 cd ..
 
